7ee14f23187af0c13a31c7aaec18d41eb3e8d6d0
shahidseeroo <shahid.seeroo@gmail.com>
2020-08-28 09:35:59 +0530

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HelloComponent } from './hello.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { SecondComponent } from './second/second.component';
 import {UserModule} from './user/user.module';
-import { ForLoopComponent } from './for-loop/for-loop.component'
+import { ForLoopComponent } from './for-loop/for-loop.component';
+import { StyleBindingComponent } from './style-binding/style-binding.component';
+import { FormComponent } from './form/form.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, FirstComponentComponent, SecondComponent, ForLoopComponent],
+  declarations: [ AppComponent, HelloComponent, FirstComponentComponent, SecondComponent, ForLoopComponent, StyleBindingComponent, FormComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/style-binding/style-binding.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-style-binding',
+  templateUrl: './style-binding.component.html',
+  styleUrls: ['./style-binding.component.css']
+})
+export class StyleBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  colorvar='black'
+
+  changeColor(color){
+    this.colorvar=color;
+
+  }
+}
